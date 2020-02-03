001fdb3f09b1beb5213cc51345c243ea5091e425
nm180612 <nicolai.mathar@ima.rwth-aachen.de>
2020-02-03 14:05:38 +0100

    add check for is  master

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,17 @@ FROM python:3-buster
 ENV DEBIAN_FRONTEND noninteractive
 ENV TZ Europe/Berlin
 
+RUN wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
+RUN echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+RUN apt-get update && apt-get install -y mongodb-org-shell
+
+
 # Installing Mongo Connector which will connect MongoDB and Elasticsearch
-RUN pip install 'mongo-connector[elastic5]==2.5.1'
+RUN pip install 'mongo-connector[elastic5]==3.1.1'
 
 COPY startup.sh /tmp/
+
+RUN chmod +x /tmp/startup.sh
 
 VOLUME /data
 
--- startup.sh
+++ startup.sh
@@ -4,6 +4,22 @@ mongo="${MONGO}"
 elasticsearch="${ELASTICSEARCH:-elasticsearch}"
 elasticport="${ELASTICPORT:-9200}"
 
+is_master_result="false"
+expected_result="true"
+
+while true;
+do
+  if [ "$is_master_result" != "$expected_result" ]; then
+    is_master_result=`mongo ${mongo} --quiet --eval "d=db.isMaster(); print( d['ismaster'] );"`
+    echo "Waiting for Mongod node to assume primary status..."
+    sleep 3
+  else
+    echo "Mongod node is now primary"
+    break;
+  fi
+done
+
+sleep 1
 
 mongo-connector --auto-commit-interval=0 --oplog-ts=/data/oplog.ts -m ${mongo} -t ${elasticsearch}:${elasticport} -d elastic2_doc_manager
 
