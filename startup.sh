5ea03fb50134b1bab92e7cfa372b0ecdae47e420
NMathar <8554558+NMathar@users.noreply.github.com>
2020-02-18 12:00:02 +0100

    add --continue-on-error flag

--- startup.sh
+++ startup.sh
@@ -21,6 +21,6 @@ done
 
 sleep 1
 
-mongo-connector --auto-commit-interval=0 --oplog-ts=/data/oplog.ts -m ${mongo} -t ${elasticsearch}:${elasticport} -d elastic2_doc_manager
+mongo-connector --continue-on-error --auto-commit-interval=0 --oplog-ts=/data/oplog.ts -m ${mongo} -t ${elasticsearch}:${elasticport} -d elastic2_doc_manager
 
 cat /mongo-connector.log
